5416db8842d1283eb1ee6558c623508c1ebba1df
Dillon Parker <prkr93@gmail.com>
2022-02-03 18:16:32 -0700

    Add initial tests and logic for takeTurn method for Round class

--- src/Round.js
+++ src/Round.js
@@ -1,10 +1,19 @@
 class Round {
-  constructor(currentCard) {
-    this.currentCard = currentCard;
+  constructor(turn) {
+    this.currentCard = turn.card;
     this.incorrectCards = [];
+    this.turnID = 1;
+    this.turn = turn;
   }
   returnCard() {
     return this.currentCard;
+  }
+  takeTurn() {
+    if(!this.turn.evaluateGuess()) {
+      this.incorrectCards.push(this.currentCard.id);
+    }
+    this.turn.giveFeedback();
+    this.turnID += 1;
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -4,22 +4,31 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
+const data = require('../src/data');
 
 describe('Round', function() {
 
   it('should be a function', function() {
-    const round = new Round();
+    const card = new Card(1, 'What is my favorite color?', ['green', 'blue', 'red'], 'blue');
+    const guess = 'red';
+    const turn = new Turn(guess, card);
+    const round = new Round(turn);
     expect(Round).to.be.a('function');
   });
 
-  it('should be an instance of Deck', function() {
-    const round = new Round();
+  it('should be an instance of Round', function() {
+    const card = new Card(1, 'What is my favorite color?', ['green', 'blue', 'red'], 'blue');
+    const guess = 'red';
+    const turn = new Turn(guess, card);
+    const round = new Round(turn);
     expect(round).to.be.an.instanceof(Round);
   });
 
   it('should return the current card', function() {
-    const card = new Card(1);
-    const round = new Round(card);
+    const card = new Card(1, 'What is my favorite color?', ['green', 'blue', 'red'], 'blue');
+    const guess = 'red';
+    const turn = new Turn(guess, card);
+    const round = new Round(turn);
     expect(round.returnCard()).to.equal(card);
   });
 
@@ -27,15 +36,28 @@ describe('Round', function() {
   //new turn instance is created, update turn count,
   //next card becomes current, evaluate guess, stores id of incorrect,
   //give feedback
-
+    const card = new Card(1, 'What is my favorite color?', ['green', 'blue', 'red'], 'blue');
+    const guess = 'blue';
+    const turn = new Turn(guess, card);
+    const round = new Round(turn);
+    round.takeTurn();
+    expect(round.turnID).to.equal(2);
   });
 
-  it('should be able to calculate and return the percent correct', function() {
-
+  it('should be able to store incorrect guesses', function() {
+    const card = new Card(1, 'What is my favorite color?', ['green', 'blue', 'red'], 'blue');
+    const guess = 'red';
+    const turn = new Turn(guess, card);
+    const round = new Round(turn);
+    round.takeTurn();
+    expect(round.incorrectCards.length).to.equal(1);
   });
-
-  it('should print something when the round is over', function() {
-
-  });
+  // it('should be able to calculate and return the percent correct', function() {
+  //
+  // });
+  //
+  // it('should print something when the round is over', function() {
+  //
+  // });
 
 });
